1bfd69a50757cb8428d2d2fbfa88723cfb72bdc5
Peter Bui <pbui@nd.edu>
2026-08-30 00:05:47 -0400

    scripts/watch: rate limit

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+PREV_TIMESTAMP=0
+
 inotifywait -e close_write -r -m $(pwd) | \
     while read path action file; do
     	case $file in
-    	    *.md|*.css|*.yaml) make install;;
+    	    *.md|*.css|*.yaml)
+    	    CURR_TIMESTAMP=$(date +%s)
+	    if [ $(($CURR_TIMESTAMP - $PREV_TIMESTAMP)) -lt 2 ]; then
+	    	continue
+	    fi
+	    PREV_TIMESTAMP=$CURR_TIMESTAMP
+    	    make install
+    	    ;;
     	esac
     done
